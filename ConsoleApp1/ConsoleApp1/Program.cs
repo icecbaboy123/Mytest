c33b209fc6e64306c3e4a5b18fc344272fbbd794
Ben <349927211@qq.com>
2020-09-07 15:08:29 +0800

    上传github

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我的第一个项目上传github");
+
+            Console.ReadKey();
+        }
+    }
+}
